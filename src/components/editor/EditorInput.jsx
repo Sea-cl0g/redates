3f958beba2758837b9814e50ae92a3a271b0f458
boo_manKnow <119836405+Sea-cl0g@users.noreply.github.com>
2026-01-05 23:06:16 +0900

    feat: エディタ入力コンポーネントを実装し、メッセージと日付の入力機能を追加

--- src/components/editor/EditorInput.jsx
+++ src/components/editor/EditorInput.jsx
@@ -1,22 +1,60 @@
 import React from 'react';
-import { Input, Flex } from 'antd';
+import { AndroidOutlined, AppleOutlined } from '@ant-design/icons';
+import { Input, Flex, Tabs, Divider } from 'antd';
+
+const onChange = key => {
+    console.log(key);
+};
+
+function EditorInputMessage() {
+    return (
+        <Input.TextArea rows={3} placeholder="Input Message" allowClear onChange={onChange} />
+    );
+};
+
+function EditorInputDate() {
+    return (
+        <Flex vertical style={{ flex: 1 }}>
+            <Tabs
+                defaultActiveKey="1"
+                items={[
+                    {
+                        label: 'Plain Text',
+                        key: '1',
+                    },
+                    {
+                        label: 'Json',
+                        key: '2',
+                        disabled: true
+                    },
+                    {
+                        label: 'YAML',
+                        key: '3',
+                        disabled: true
+                    },
+                ]}
+                onChange={onChange}
+            />
+            <Input.TextArea style={{ flex: 1, resize: 'none' }} placeholder="Input Date" allowClear onChange={onChange}/>
+        </Flex>
+    );
+}
 
 
-export default function App() {
+export default function EditorInput() {
     const padding = 16;
     return (
         <Flex
             vertical
-            gap="middle"
+            gap="small"
             style={{
                 height: `calc(100% - ${padding * 2}px)`,
                 padding: `${padding}px`,
             }}
         >
-            <Input placeholder="ご連絡ありがとうございます。それでは下記日程でお願いします。" />
-            <Input.TextArea
-                style={{ flex: 1, resize: 'none' }}
-            />
+            <EditorInputMessage />
+            <Divider />
+            <EditorInputDate />
         </Flex>
     );
 };
