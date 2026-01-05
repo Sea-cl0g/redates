a9cb7ce18e382c36c41f9dd7fe99596f237b14d7
boo_manKnow <119836405+Sea-cl0g@users.noreply.github.com>
2026-01-05 18:29:18 +0900

    fix: Flexコンポーネントにjs式が埋め込まれない問題を修正

--- src/components/editor/EditorInput.jsx
+++ src/components/editor/EditorInput.jsx
@@ -5,7 +5,14 @@ import { Input, Flex } from 'antd';
 export default function App() {
     const padding = 16;
     return (
-        <Flex vertical gap="middle" style={{ height: 'calc(100% - ${padding * 2}px)', padding: '${padding}px' }}>
+        <Flex
+            vertical
+            gap="middle"
+            style={{
+                height: `calc(100% - ${padding * 2}px)`,
+                padding: `${padding}px`,
+            }}
+        >
             <Input placeholder="ご連絡ありがとうございます。それでは下記日程でお願いします。" />
             <Input.TextArea
                 style={{ flex: 1, resize: 'none' }}
