f755804b8e8c24cc6851a39a6026fb19fa81ca9e
boo_manKnow <119836405+Sea-cl0g@users.noreply.github.com>
2026-01-06 12:15:26 +0900

    feat: EditorOutputを分割し、出力用のボックスを用意

--- src/components/editor/EditorOutput.jsx
+++ src/components/editor/EditorOutput.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { PoweroffOutlined, SyncOutlined } from '@ant-design/icons';
 import { Flex, Switch, Typography, Button, Divider } from 'antd';
+const { Title, Paragraph, Text, Link } = Typography;
 
-
-export default function EditorOutput() {
-    const padding = 16;
+function EditorOutputHeader() {
     const [isAiEnabled, setIsAiEnabled] = useState(false);
     const changeIsAiEnabled = value => {
         setIsAiEnabled(value);
@@ -29,29 +28,74 @@ export default function EditorOutput() {
     };
 
     return (
+        <Flex justify="space-between" align="center">
+            <Flex justify="flex-start" align="center" gap="small">
+                <Text strong>AI Mode</Text>
+                <Divider orientation="vertical" />
+                <Switch
+                    checked={isAiEnabled}
+                    onChange={changeIsAiEnabled}
+                    checkedChildren="ON"
+                    unCheckedChildren="OFF"
+                />
+            </Flex>
+            <Button type="primary" disabled={!isAiEnabled} loading={loadings[0]} onClick={() => enterLoading(0)}>
+                Generate
+            </Button>
+        </Flex>
+    );
+}
+
+function EditorOutputMain() {
+    const outputStyle = {
+        flex: 1,
+        height: `100%`,
+        borderRadius: 6,
+        border: '1px solid #d9d9d9',
+    }
+
+    return (
+        <Flex
+            style={outputStyle}
+        >
+            <Flex style={{ padding: 6 }}>
+                <Typography.Paragraph>
+                    この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字
+                </Typography.Paragraph>
+            </Flex>
+        </Flex>
+    );
+}
+
+function EditorOutputFooter() {
+    return (
+        <Flex>
+            <p>test</p>
+        </Flex>
+    );
+}
+
+export default function EditorOutput() {
+    const padding = 16;
+    return (
         <Flex
             vertical
-            gap="small"
+            justify="space-between"
             style={{
                 height: `calc(100% - ${padding * 2}px)`,
                 padding: `${padding}px`,
             }}
         >
-            <Flex justify="space-between" align="center">
-                <Flex justify="flex-start" align="center" gap="small">
-                    <Typography.Text strong>AI Mode</Typography.Text>
-                    <Divider orientation="vertical" />
-                    <Switch
-                        checked={isAiEnabled}
-                        onChange={changeIsAiEnabled}
-                        checkedChildren="ON"
-                        unCheckedChildren="OFF"
-                    />
-                </Flex>
-                <Button type="primary" disabled={!isAiEnabled} loading={loadings[0]} onClick={() => enterLoading(0)}>
-                    Generate
-                </Button>
+            <Flex
+                vertical
+                justify="flex-start"
+                style={{ flex: 1 }}
+                gap="small"
+            >
+                <EditorOutputHeader />
+                <EditorOutputMain />
             </Flex>
+            <EditorOutputFooter />
         </Flex>
     );
 };
