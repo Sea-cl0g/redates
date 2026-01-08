0358cba4cf828b611692fca33c85fe9f40f281e5
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-01-08 23:27:21 +0900

    update: ソースコードへのリンクを追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ export default function Footer() {
   return (
     <footer>
       <Flex justify="space-between" align="center">
-        <Text>test</Text>
+        <Link href="https://github.com/Sea-cl0g/redates/tree/main" target="_blank">
+          GitHub
+        </Link>
         <Text>日本大学文理学部情報科学科 Webプログラミングの演習課題 (宍戸佑匡 5424085)</Text>
       </Flex>
     </footer>
