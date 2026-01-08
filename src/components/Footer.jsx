e8f6a8e4bbf8bce4e98517f1c8d0476bdc828f6e
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-01-09 02:35:41 +0900

    update: Githubへのリンクを削除

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,7 @@ export default function Footer() {
   return (
     <footer>
       <Flex justify="space-between" align="center">
-        <Link href="https://github.com/Sea-cl0g/redates/tree/main" target="_blank">
-          GitHub
-        </Link>
+        <Text></Text>
         <Text>日本大学文理学部情報科学科 Webプログラミングの演習課題 (宍戸佑匡 5424085)</Text>
       </Flex>
     </footer>
