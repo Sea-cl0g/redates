2d41634ebd383019680b0e7648530cc4f50295f5
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-01-08 23:25:37 +0900

    add: フッターの実装

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
+import { Flex, Typography } from 'antd';
+const { Title, Paragraph, Text, Link } = Typography;
+
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer>
+      <Flex justify="space-between" align="center">
+        <Text>test</Text>
+        <Text>日本大学文理学部情報科学科 Webプログラミングの演習課題 (宍戸佑匡 5424085)</Text>
+      </Flex>
     </footer>
   );
 }
