e286e5bf68b0d96928779b35cd43e760229b6eb0
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-01-22 01:45:07 +0900

    test: commitをフッターに埋め込み

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer>
       <Flex justify="space-between" align="center">
-        <Text></Text>
+        <Text>{commit || 'empty'}</Text>
         <Text>日本大学文理学部情報科学科 Webプログラミングの演習課題 (宍戸佑匡 5424085)</Text>
       </Flex>
     </footer>
